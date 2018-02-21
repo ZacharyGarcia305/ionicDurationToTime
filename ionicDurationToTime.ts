bcea0112f5221f03764882327c5a181082707a8d
ZacharyGarcia305 <32903144+ZacharyGarcia305@users.noreply.github.com>
2018-02-21 09:21:48 -0500

    duration to time

---
+++ ionicDurationToTime.ts
@@ -0,0 +1,96 @@
+
+export class ionicDurationToTime
+{
+    hourFormat ( decimalTime )
+    {
+        if ( decimalTime < 10 && decimalTime.substr( 1, 1 ) != "." )
+            decimalTime = "0" + decimalTime.substr( 0, 1 ) + ":00";
+
+        if ( decimalTime >= 10 && decimalTime.substr( 2, 2 ).substr( 0, 1 ) != "." )
+            decimalTime = decimalTime.substr( 0, 2 ).toString() + ":00";
+
+        if (
+            decimalTime < 10 &&
+            decimalTime.substr( 2, 2 ).substr( 0, 1 ) >= 1 &&
+            decimalTime.substr( 2, 2 ).substr( 1, 2 ) < 1
+        )
+            decimalTime =
+                "0" +
+                decimalTime.substr( 0, 1 ).toString() +
+                ":" +
+                decimalTime
+                    .substr( 2, 2 )
+                    .substr( 0, 1 )
+                    .toString() +
+                "0";
+
+        if (
+            decimalTime < 10 &&
+            decimalTime.substr( 2, 2 ).substr( 0, 1 ) >= 1 &&
+            decimalTime.substr( 2, 2 ).substr( 1, 2 ) >= 1
+        )
+            decimalTime =
+                "0" +
+                decimalTime.substr( 0, 1 ).toString() +
+                ":" +
+                decimalTime
+                    .substr( 2, 2 )
+                    .substr( 0, 2 )
+                    .toString();
+
+        if (
+            decimalTime >= 10 &&
+            decimalTime.substr( 3, 3 ).substr( 0, 1 ) >= 1 &&
+            decimalTime.substr( 4, 4 ).substr( 0, 1 ) < 1
+        )
+            decimalTime =
+                decimalTime.substr( 0, 2 ).toString() +
+                ":" +
+                decimalTime
+                    .substr( 3, 3 )
+                    .substr( 0, 1 )
+                    .toString() +
+                "0";
+
+        if (
+            decimalTime >= 10 &&
+            decimalTime.substr( 3, 3 ).substr( 0, 1 ) >= 1 &&
+            decimalTime.substr( 4, 4 ).substr( 0, 1 ) >= 1
+        )
+            decimalTime =
+                decimalTime.substr( 0, 2 ).toString() +
+                ":" +
+                decimalTime
+                    .substr( 3, 4 )
+                    .substr( 0, 2 )
+                    .toString();
+
+        if (
+            decimalTime >= 10 &&
+            decimalTime.substr( 3, 3 ).substr( 0, 1 ) < 1 &&
+            decimalTime.substr( 4, 4 ).substr( 0, 1 ) >= 1
+        )
+            decimalTime =
+                decimalTime.substr( 0, 2 ).toString() +
+                ":" +
+                decimalTime
+                    .substr( 3, 4 )
+                    .substr( 0, 2 )
+                    .toString();
+
+        if (
+            decimalTime < 10 &&
+            decimalTime.substr( 2, 2 ).substr( 0, 1 ) < 1 &&
+            decimalTime.substr( 2, 2 ).substr( 1, 2 ) >= 1
+        )
+            decimalTime =
+                "0" +
+                decimalTime.substr( 0, 1 ).toString() +
+                ":" +
+                decimalTime
+                    .substr( 2, 2 )
+                    .substr( 0, 2 )
+                    .toString();
+        return decimalTime.toString();
+    }
+}
